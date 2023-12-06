b628eec62c7face75e25f4126e1d82f52e669136
Christian Marcelo <christian.marcelo.castro@gmail.com>
2023-12-05 21:25:41 -0400

    updating optional field

--- client/src/tools/data.model.ts
+++ client/src/tools/data.model.ts
@@ -1,16 +1,17 @@
 export interface Technology {
-    _id:         string;
-    name:        string;
+    _id: string;
+    name: string;
     description: string;
-    difficulty:  number;
-    courses:     Course[];
+    difficulty: number;
+    courses: Course[];
 }
 
 export interface Course {
+    _id?: string;
     code: string;
     name: string;
 }
 
 export interface ComponentProps {
-    technologies:Technology[];
+    technologies: Technology[];
 }
