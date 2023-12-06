826475d1992ff469be52dc5238b0545f42b2b405
Christian Marcelo <christian.marcelo.castro@gmail.com>
2023-12-05 22:37:40 -0400

    adding fontawesome

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -2,6 +2,8 @@ import { Technology, Course } from "./../tools/data.model";
 import { getAllData } from "../tools/DataManager";
 import { useEffect, useState } from "react";
 import { NextRouter, useRouter } from "next/router";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home({ technologies, courses }: { technologies: Technology[], courses: Course[] }) {
 
@@ -12,16 +14,24 @@ export default function Home({ technologies, courses }: { technologies: Technolo
 
 
   return (
-    <div className="flex flex-wrap justify-evenly">
+    <div className="flex flex-wrap justify-around">
       <div>
         {techs.map((technoogy: Technology, n: number) =>
-          <div key={n} className="my-4">{technoogy.name}</div>
+          <div className="flex gap-2 items-center">
+            <FontAwesomeIcon icon={faPenToSquare} onClick={() => router.push("/add")} className="cursor-pointer" />
+            <FontAwesomeIcon icon={faTrash} onClick={() => router.push("/delete")} className="cursor-pointer" />
+            <div key={n} className="my-4">{technoogy.name}</div>
+          </div>
         )}
 
       </div>
       <div>
         {updCourses.map((course: Course, n: number) =>
-          <div key={n} className="my-4">{course.name}</div>
+          <div className="flex gap-2 items-center">
+            <FontAwesomeIcon icon={faPenToSquare} onClick={() => router.push("/add")} className="cursor-pointer" />
+            <FontAwesomeIcon icon={faTrash} onClick={() => router.push("/delete")} className="cursor-pointer" />
+            <div key={n} className="my-4">{course.code} | {course.name}</div>
+          </div>
         )}
 
       </div>
