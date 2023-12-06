719592778a92b8e0fca0346fae087c8e64099539
Christian Marcelo <christian.marcelo.castro@gmail.com>
2023-12-05 21:41:26 -0400

    displaying both  arrays in independent divs

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,15 +1,33 @@
 import { Technology, Course } from "./../tools/data.model";
 import { getAllData } from "../tools/DataManager";
+import { useEffect, useState } from "react";
 
 export default function Home({ technologies, courses }: { technologies: Technology[], courses: Course[] }) {
 
+  const [techs, setTechs] = useState<Technology[]>(technologies);
+  const [updCourses, setUpdCourses] = useState<Course[]>(courses);
 
 
   return (
-    <div className="font-bold text-sm p-4">
-      <pre>
-        {JSON.stringify(technologies, null, "\t")}
-      </pre>
+    // <div className="font-bold text-sm p-4">
+    //   <pre>
+    //     {JSON.stringify(technologies, null, "\t")}
+    //   </pre>
+    // </div>
+    <div className="flex flex-wrap">
+      <div>
+        {techs.map((technoogy: Technology, n: number) =>
+          <div key={n}>{technoogy.name}</div>
+        )}
+
+      </div>
+      <div>
+        {updCourses.map((course: Course, n: number) =>
+          <div key={n}>{course.name}</div>
+        )}
+
+      </div>
+
     </div>
   )
 }
