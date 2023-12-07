35487b90dbc291ff1cb671c60ea93879d85aef85
Christian Marcelo <christian.marcelo.castro@gmail.com>
2023-12-06 21:06:50 -0400

    adding a pointer event to add course

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,9 +1,9 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare, faTrash, faSquarePlus } from "@fortawesome/free-solid-svg-icons";
 import { Technology, Course } from "./../tools/data.model";
 import { getAllData } from "../tools/DataManager";
 import { useEffect, useState } from "react";
 import { NextRouter, useRouter } from "next/router";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPenToSquare, faTrash, faSquarePlus } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home({ technologies, courses }: { technologies: Technology[], courses: Course[] }) {
 
@@ -46,7 +46,7 @@ export default function Home({ technologies, courses }: { technologies: Technolo
 
       </div>
       <div>
-        <FontAwesomeIcon icon={faSquarePlus} className="text-xl" onClick={addNewCourse} />
+        <FontAwesomeIcon icon={faSquarePlus} className="text-xl cursor-pointer" onClick={addNewCourse} />
         {updCourses.map((course: Course, n: number) =>
           <div className="flex gap-2 items-center">
             <FontAwesomeIcon icon={faPenToSquare} onClick={() => router.push("/edit")} className="cursor-pointer" />
