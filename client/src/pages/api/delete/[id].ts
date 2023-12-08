e41d557355af3ec2f0332465093f7b88965e58b8
Christian Marcelo <christian.marcelo.castro@gmail.com>
2023-12-08 06:02:24 -0400

    fixing request.method because it was not DELETE :(

--- client/src/pages/api/delete/[id].ts
+++ client/src/pages/api/delete/[id].ts
@@ -1,8 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { deleteTechnology } from "@/tools/DataManager";
 
-export default async function handler( request: NextApiRequest, response: NextApiResponse<any>) {
-    if (request.method == "POST") {
+export default async function handler(request: NextApiRequest, response: NextApiResponse<any>) {
+    if (request.method == "DELETE") {
         await deleteTechnology(request, response);
     }
 }
